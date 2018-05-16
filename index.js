a1b0019de373123f17c699f73c1211b7b0c85e82
Andrew Yates <andrew_yates@rocketmail.com>
2018-05-16 01:50:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ function breakOut(array, changeValue, stopValue) {
   }
 }
 
-function keepGoing(array, changeValue, stopValue) {
+function keepGoing(array, changeValue, skipValue) {
   for(var i = 0; i < array.length; i++) {
-    
+    if (array[i] === skipValue) {
+      continue;
+    }
+    array[i] = changeValue;
   }
 }
