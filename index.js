97fb88bade571bc56327a37af4197d1893299718
Andrew Yates <andrew_yates@rocketmail.com>
2018-05-16 02:40:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
 	for (let i = 0; i < array.length; i++) {
-		if(array[i] === findFn()) {
-			return findFn();
+		if (findFn(array[i])) {
+			return array[i]
         }
-    } 
-    return null;
+    }
+	return null;
 }
 
